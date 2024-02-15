92647ac6d8554255b22231e1b686a9c873865e94
Seunghyeok Kim <139187561+Seunghyeok8745@users.noreply.github.com>
2024-02-15 08:48:53 -0800

    Delete Example Test/example.js

--- Example Test/example.js
+++
@@ -1,7 +0,0 @@
-console.log(document.getElementById('test').innerHTML);
-console.log(document.getElementById('test2').textContent);
-
-document.getElementById('test').innerHTML = `<h1> brother</h1>`;
-// 효과 까지 적용
-document.getElementById('test2').textContent = `<h1> brother</h1>`;
-// text를 그대로 전환
